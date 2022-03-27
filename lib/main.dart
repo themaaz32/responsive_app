31e4f71d7f81268f3c4faceb1a5edfa7df8918f5
swayamshreemohanty <mohantyswayamshree@gmail.com>
2022-03-27 19:23:50 +0530

    SingleChildScrollView added to column

--- lib/main.dart
+++ lib/main.dart
@@ -63,141 +63,143 @@ class HomePage extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 24.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 52.h,
-              ),
-              Text(
-                "Sign Up to Masterminds",
-                style: TextStyle(
-                  fontSize: 18.sp,
-                  fontWeight: FontWeight.w700,
-                  color: darkTextColor,
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 52.h,
                 ),
-              ),
-              SizedBox(
-                height: 4.h,
-              ),
-              Wrap(
-                children: [
-                  Text(
-                    "Already have an account? ",
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w600,
-                      color: lightTextColor,
-                    ),
+                Text(
+                  "Sign Up to Masterminds",
+                  style: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w700,
+                    color: darkTextColor,
                   ),
-                  Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w700,
-                      color: purpleColor,
+                ),
+                SizedBox(
+                  height: 4.h,
+                ),
+                Wrap(
+                  children: [
+                    Text(
+                      "Already have an account? ",
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w600,
+                        color: lightTextColor,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 24.h,
-              ),
-              getTextField(hint: "Full Name"),
-              SizedBox(
-                height: 16.h,
-              ),
-              getTextField(hint: "Email"),
-              SizedBox(
-                height: 16.h,
-              ),
-              getTextField(hint: "Password"),
-              SizedBox(
-                height: 16.h,
-              ),
-              getTextField(hint: "Confirm Password"),
-              SizedBox(
-                height: 16.h,
-              ),
-              SizedBox(
-                width: double.infinity,
-                child: TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(purpleColor),
-                      foregroundColor: MaterialStateProperty.all(Colors.white),
-                      padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(vertical: 14.h)),
-                      textStyle: MaterialStateProperty.all(TextStyle(
+                    Text(
+                      "Login",
+                      style: TextStyle(
                         fontSize: 14.sp,
                         fontWeight: FontWeight.w700,
-                      ))),
-                  child: Text("Create Account"),
+                        color: purpleColor,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 16.h,),
-              Row(
-                children: [
-                  Expanded(child: Divider()),
-                  SizedBox(width: 16.w,),
-                  Text(
-                    "or sign up via",
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500,
-                      color: lightTextColor,
+                SizedBox(
+                  height: 24.h,
+                ),
+                getTextField(hint: "Full Name"),
+                SizedBox(
+                  height: 16.h,
+                ),
+                getTextField(hint: "Email"),
+                SizedBox(
+                  height: 16.h,
+                ),
+                getTextField(hint: "Password"),
+                SizedBox(
+                  height: 16.h,
+                ),
+                getTextField(hint: "Confirm Password"),
+                SizedBox(
+                  height: 16.h,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all(purpleColor),
+                        foregroundColor: MaterialStateProperty.all(Colors.white),
+                        padding: MaterialStateProperty.all(
+                            EdgeInsets.symmetric(vertical: 14.h)),
+                        textStyle: MaterialStateProperty.all(TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w700,
+                        ))),
+                    child: Text("Create Account"),
+                  ),
+                ),
+                SizedBox(height: 16.h,),
+                Row(
+                  children: [
+                    Expanded(child: Divider()),
+                    SizedBox(width: 16.w,),
+                    Text(
+                      "or sign up via",
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w500,
+                        color: lightTextColor,
+                      ),
+                    ),
+                    SizedBox(width: 16.w,),
+                    Expanded(child: Divider()),
+                  ],
+                ),
+                SizedBox(height: 16.h,),
+                SizedBox(
+                  width: double.infinity,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: ButtonStyle(
+                      side: MaterialStateProperty.all(BorderSide(color: borderColor,)),
+                        foregroundColor: MaterialStateProperty.all(darkTextColor),
+                        padding: MaterialStateProperty.all(
+                            EdgeInsets.symmetric(vertical: 14.h)),
+                        textStyle: MaterialStateProperty.all(TextStyle(
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w700,
+                        ))),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Image.asset("assets/Google.png"),
+                        SizedBox(width : 10.w),
+                        Text("Google"),
+                      ],
                     ),
                   ),
-                  SizedBox(width: 16.w,),
-                  Expanded(child: Divider()),
-                ],
-              ),
-              SizedBox(height: 16.h,),
-              SizedBox(
-                width: double.infinity,
-                child: TextButton(
-                  onPressed: () {},
-                  style: ButtonStyle(
-                    side: MaterialStateProperty.all(BorderSide(color: borderColor,)),
-                      foregroundColor: MaterialStateProperty.all(darkTextColor),
-                      padding: MaterialStateProperty.all(
-                          EdgeInsets.symmetric(vertical: 14.h)),
-                      textStyle: MaterialStateProperty.all(TextStyle(
+                ),
+                SizedBox(height: 16.h,),
+                Wrap(
+                  children: [
+                    Text(
+                      "By signing up to Masterminds you agree to our ",
+                      style: TextStyle(
+                        fontSize: 14.sp,
+                        fontWeight: FontWeight.w600,
+                        color: lightTextColor,
+                      ),
+                    ),
+                    Text(
+                      "terms and conditions",
+                      style: TextStyle(
                         fontSize: 14.sp,
                         fontWeight: FontWeight.w700,
-                      ))),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Image.asset("assets/Google.png"),
-                      SizedBox(width : 10.w),
-                      Text("Google"),
-                    ],
-                  ),
+                        color: purpleColor,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              SizedBox(height: 16.h,),
-              Wrap(
-                children: [
-                  Text(
-                    "By signing up to Masterminds you agree to our ",
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w600,
-                      color: lightTextColor,
-                    ),
-                  ),
-                  Text(
-                    "terms and conditions",
-                    style: TextStyle(
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w700,
-                      color: purpleColor,
-                    ),
-                  ),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
